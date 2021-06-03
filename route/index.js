cee9a5e13b4747a10861aefa546a702f52496f97
wuxb <vence2013@163.com>
2021-06-03 22:14:08 +0800

    验证实现rpio的管脚控制

--- route/index.js
+++ route/index.js
@@ -1,6 +1,9 @@
 const Router = require('koa-router');
 var router = new Router();
+var rpio = require('rpio');
 
+
+rpio.open(16, rpio.OUTPUT, rpio.LOW);
 
 router.get('/', async (ctx)=>{
     await ctx.render('view/index.html', {'name':'rpi controller'}); 
@@ -8,8 +11,13 @@ router.get('/', async (ctx)=>{
 
 router.get('/gpio/:gpiox', async (ctx)=>{
     let params = ctx.params;
+    let query  = ctx.query;
     
-    console.log(params);
+    let gpio = parseInt(params.gpiox);
+    let value= parseInt(query.set);
+    //console.log(gpio, value);
+    rpio.write(gpio, value ? rpio.HIGH : rpio.LOW);
+    
     ctx.body = 'SUCCESS';
 })
 
