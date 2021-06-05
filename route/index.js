52bf703ff25e7da9bdf57763bf178fc86c483918
wuxb <vence2013@163.com>
2021-06-05 22:58:28 +0800

    ss

--- route/index.js
+++ route/index.js
@@ -3,39 +3,23 @@ const Router = require('koa-router');
 var router = new Router();
 
 
-router.get('/', async (ctx)=>{
+router.get('/', async (ctx) => 
+{
     await ctx.render('view/index.html', {'name':'rpi controller'}); 
 })
 
-router.get('/control/:type', async (ctx)=>{
+router.get('/control/:type', async (ctx) => 
+{
     let params = ctx.params;    
     let type = params.type;
 
     Device.toggle( type );    
-    ctx.body = Device.status();;
+    ctx.body = Device.status();
 })
 
-/*
-router.get('/gpio/:gpiox', async (ctx)=>{
-    let params = ctx.params;
-    let query  = ctx.query;
-    
-    let gpio = parseInt(params.gpiox);
-    let value= parseInt(query.set);
-    rpio.write(gpio, value ? rpio.HIGH : rpio.LOW);
-    
-    ctx.body = 'SUCCESS';
+router.get('/status', async (ctx) => 
+{
+    ctx.body = Device.status();
 })
-
-router.get('/status', async (ctx)=>{
-
-    let water = rpio.read(27);
-    let solid = rpio.read(28);
-
-    let res = {'water':water, 'solid':solid};
-    console.log(res);
-    ctx.body = res;
-})
-*/
 
 module.exports = router;
--- view/js/index.js
+++ view/js/index.js
@@ -1,49 +1,29 @@
 function direct_ctl(type)
 {
-    $.get("/control/"+type, (res) => {
-        console.log(res);
-
-        $(".direct_ctl").find(".btn")
-        .removeClass("btn-secondary btn-success");
-        $("#pump").addClass(res['pump'] ? 'btn-success' : 'btn-secondary');
-        $("#irrigate").addClass(res['irrigate'] ? 'btn-success' : 'btn-secondary');
-
-        /* 输入状态 */
-        $(".input_status").find(".alert")
-        .removeClass("alert-secondary alert-warning");
-
-        $("#waterfull")
-        .text(res['waterfull'] ? '水已满' : '水未满')
-        .addClass(res['waterfull'] ? 'alert-warning' : 'alert-secondary');
-
-        $("#soilwet")
-        .text(res['soilwet'] ? '湿度已够' : '湿度不足')
-        .addClass(res['soilwet'] ? 'alert-warning' : 'alert-secondary');
-    });
+    $.get("/control/"+type, (res) => { display_status(res); });
 }
 
-function gpio_set(pin)
+function display_status(status)
 {
-    let val;
+    $(".direct_ctl").find(".btn")
+    .removeClass("btn-secondary btn-success");
+    $("#pump").addClass(status['pump'] ? 'btn-success' : 'btn-secondary');
+    $("#irrigate").addClass(status['irrigate'] ? 'btn-success' : 'btn-secondary');
 
-    if (15 == pin)
-        val = $("#ctl1").is(':checked') ? 1 : 0;
-    else if (16 == pin)
-        val = $("#ctl2").is(':checked') ? 1 : 0;
-    else 
-        val = $("#ctl3").is(':checked') ? 1 : 0;
+    /* 输入状态 */
+    $(".input_status").find(".alert")
+    .removeClass("alert-secondary alert-warning");
 
-    console.log(pin, val);
-    $.get("/gpio/"+pin+"?set="+val, (res) => {
-        console.log(res);
-    });
+    $("#waterfull")
+    .text(status['waterfull'] ? '水已满' : '水未满')
+    .addClass(status['waterfull'] ? 'alert-warning' : 'alert-secondary');
+
+    $("#soilwet")
+    .text(status['soilwet'] ? '湿度已够' : '湿度不足')
+    .addClass(status['soilwet'] ? 'alert-warning' : 'alert-secondary');
 }
 
-function status_query()
+function query_status()
 {
-    $.get("/status", (res) => {
-        console.log(res);
-        $("#water").text(res.water);
-        $("#solid").text(res.solid);
-    });
+    $.get("/status", (res) => { display_status(res); });
 }
