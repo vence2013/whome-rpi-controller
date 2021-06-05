5f9c204b4b45f563bd1ddabb47653565b86563bd
wuxb <vence2013@163.com>
2021-06-05 21:35:14 +0800

    a

---
+++ view/js/index.js
@@ -0,0 +1,25 @@
+function gpio_set(pin)
+{
+    let val;
+
+    if (15 == pin)
+        val = $("#ctl1").is(':checked') ? 1 : 0;
+    else if (16 == pin)
+        val = $("#ctl2").is(':checked') ? 1 : 0;
+    else 
+        val = $("#ctl3").is(':checked') ? 1 : 0;
+
+    console.log(pin, val);
+    $.get("/gpio/"+pin+"?set="+val, (res) => {
+        console.log(res);
+    });
+}
+
+function status_query()
+{
+    $.get("/status", (res) => {
+        console.log(res);
+        $("#water").text(res.water);
+        $("#solid").text(res.solid);
+    });
+}
